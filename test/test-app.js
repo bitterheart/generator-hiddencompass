9f1cc8324cdf185146b00115fe42fd9bf1aed361
Emory Merryman <emory.merryman@gmail.com>
2014-12-09 07:01:59 -0500

    added test

--- test/test-app.js
+++ test/test-app.js
@@ -1,27 +1,25 @@
-'use strict';
-
-var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
-var os = require('os');
-
-describe('hiddencompass:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
-      .withPrompt({
-        someOption: true
-      })
-      .on('end', done);
-  });
-
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
-  });
-});
+(function(){
+    'use strict';
+    var path = require('path');
+    var assert = require('yeoman-generator').assert;
+    var helpers = require('yeoman-generator').test;
+    var os = require('os');
+    describe("hiddencompass",function(){
+        describe("without sample project",function(){
+            before(function (done) {
+                helpers.run(path.join(__dirname, '../app'))
+                    .inDir(path.join(os.tmpdir(), './temp-test'))
+                    .withPrompt({
+                        sampleProject: false
+                    })
+                    .on('end', done);
+            });
+            it('creates files', function () {
+                assert.file([
+                    'bower.json',
+                    'package.json'
+                ]);
+            });
+        });
+    });
+}());
